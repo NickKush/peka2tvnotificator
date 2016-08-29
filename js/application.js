37bf9f7e49a9b4b9cfcbc73a3b069fec5c6e88ee
Hukutoss <hukutoss9666@gmail.com>
2016-08-29 14:42:10 +0300

    Added "to user" support

--- js/application.js
+++ js/application.js
@@ -1,14 +1,5 @@
 var app = angular.module('main', ['ngTagsInput']);
 
-var urlRegex = new RegExp('(https?:\/\/[^\\s\/$.?#].[^\\s,]*)', 'gi');
-var nicknameBoldPattern = new RegExp( '\\[b\\]([\\s\\S]+?)\\[/b\\]', 'gi');
-
-var channelsIndex = {};
-
-var smiles;
-var smilesCount;
-var smileHtmlReplacement = [];
-
 app.filter('reverse', function() {
     return function(items) {
         return items.slice().reverse();
@@ -112,7 +103,7 @@ app.controller("general", function($scope, $http, $sce) {
     function sendNotification(message) {
         var jsonData = {
             name: message.from.name,
-            message: message.text,
+            message: message.to.name !== undefined ? '[b]' + message.to.name + '[/b], ' + message.text : message.text,
             date: message.time,
             url: message.channel
         }
@@ -163,75 +154,10 @@ app.controller("general", function($scope, $http, $sce) {
 
     websocket();
 
-    function setSmiles() {
-        $.post('https://funstream.tv/api/smile', function(data) {
-            smiles = data;
-            smilesCount = smiles.length;
-            for( i = 0; i < smilesCount; i++ ) {
-                smileHtmlReplacement[i] =
-                    '<img src="' + smiles[i].url +
-                    '" width="' + smiles[i].width +
-                    '" height="' + smiles[i].height +
-                    '" class="chat-smile"/>';
-            }
-        });
-    }
-
-    function processReplaces( message ) {
-        var message = urlReplace(message);
-
-        message = message.replace( /:([-a-z0-9]{2,}):/gi, function( match, code ) {
-            var indexOfSmileWithThatCode = -1;
-            for ( var i = 0; i < smilesCount; i++ ) {
-                if ( smiles[i].code == code ) {
-                    indexOfSmileWithThatCode = i;
-                    break;
-                }
-            };
-
-            var replace = '';
-            if (indexOfSmileWithThatCode != -1) {
-                replace = smileHtmlReplacement[indexOfSmileWithThatCode];
-            } else {
-                replace = match;
-            }
-            return replace;
-        });
-
-        return message;
-    }
-
-    function urlReplace(inputText) {
-        inputText = inputText.replace(urlRegex, function(url) {
-            if(url.length > 60) {
-                return '<a href="' + url + '" target="_blank">' + url.substr(0, 45) + '...' + url.slice(-15) + '</a>';
-            }
-            return '<a href="' + url + '" target="_blank">' + url + '</a>';
-        });
-
-        inputText = inputText.replace(nicknameBoldPattern, '<b>$1</b>' );
-        return inputText;
-    }
-
-    function indexSc2Tv() {
-        console.log("Indexing Sc2Tv");
-        $.post("https://funstream.tv/api/content", {content: "stream", "type": "all", category: {slug: "top"}}, function(data){
-            for(key in data.content) {
-                stream = data.content[key];
-                if(stream.owner !== null && stream.slug !== null) {
-                    if(channelsIndex[stream.owner.id] === undefined) {
-                        channelsIndex[stream.owner.id] = stream.slug;
-                    }
-                }
-            }
-        });
-        setTimeout(indexSc2Tv, 60000);
-    }
-
     //Это называется гавнокод
     //Люби его
     function setUrl(json) {
-        if(json.url == "main") {
+        if(json.url === "main") {
             json.url = "http://funstream.tv/chat/main";
             return;
         }
@@ -255,5 +181,4 @@ app.controller("general", function($scope, $http, $sce) {
     function getChannelId(chan) {
         return chan.split('/')[1];
     }
-
 });
